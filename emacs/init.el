4e669409fb7d542d8e6df6af0d251f9b80bde47c
Hiroaki Honda <from.endor@gmail.com>
2018-11-20 01:11:03 +0900

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -39,53 +39,16 @@
 (add-to-load-path "elisp")
 
 ;; ===== exec-path =====
-;(add-to-list 'exec-path "/opt/local/bin")
-;(add-to-list 'exec-path "/opt/local/sbin")
 (add-to-list 'exec-path "/usr/local/bin")
 ;(add-to-list 'exec-path "/usr/local/sbin")
 (add-to-list 'exec-path "~/bin")
-;(add-to-list 'exec-path "/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin")
 
 ;; 環境変数 PATH に exec-path を追加する。
 (setenv "PATH" (mapconcat 'identity exec-path ":"))
 
-;; ELPA
-;(when (require 'package) 
-; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-;  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;  (package-initialize))
-
-;; ========================================
-;;   言語設定
-;; ========================================
-
-(set-language-environment "Japanese")
-(prefer-coding-system 'utf-8-unix)
-(set-default-coding-systems 'utf-8-unix)
-;; MacOSX
-(require 'ucs-normalize)
-(setq file-name-coding-system 'utf-8-hfs)
-(setq locale-coding-system 'utf-8-hfs)
-
-;; ==== 日本語入力 ====
-(setq  default-input-method "MacOSX")
-(global-set-key "\C-o" 'toggle-input-method) ; Ctrl-o で日本語入力を切り替える
-;; Control + すべてのキー　を無視する
-(when (fboundp 'mac-add-ignore-shortcut) (mac-add-ignore-shortcut '(control)))
-
-;; 親指シフトで"，"がうまく入力できないことへの対策
-;(global-unset-key (kbd "s-,"))
-(global-set-key (kbd "s-,") #'(lambda () (interactive) (insert "，")))
-
 ;;; ==== init-loader ====
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
-
-;; Emacsからの質問を y/n で回答する
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; カーソル位置のファイルパスやアドレスを "C-x C-f" で開く
-;;(ffap-bindings)
 
 ;; Emacs and Other Info files
 ;(require 'info)
@@ -98,11 +61,6 @@
        Info-default-directory-list))
 (put 'scroll-left 'disabled nil)
 
-;; Emacsの起動時間を表示
-(add-hook 'after-init-hook
-  (lambda ()
-    (message "init time: %.3f sec"
-             (float-time (time-subtract after-init-time before-init-time)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
