a2166f35492c33167a4a37a97881fa6b8084cea5
hiro0412 <from.endor@gmail.com>
2019-06-25 18:22:17 +0900

    Settings for marcurial, markdown.

---
+++ emacs/inits/06_markdown.el
@@ -0,0 +1,16 @@
+(setq markdown-command "multimarkdown")
+
+;; (autoload 'markdown-mode "markdown-mode"
+;;    "Major mode for editing Markdown files" t)
+;; (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+;; (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+  "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+(autoload 'markdown-preview-mode "markdown-preview-mode.el" t)
+;(setq markdown-preview-stylesheets (list "github.css"))
+(add-to-list 'markdown-preview-stylesheets "https://raw.githubusercontent.com/richleland/pygments-css/master/emacs.css")
+(setq markdown-preview-stylesheets (list "http://thomasf.github.io/solarized-css/solarized-light.min.css"))
+
