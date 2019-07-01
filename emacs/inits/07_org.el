9f3f2785c22317b6023d6c36984b982b375dea76
hiro0412 <from.endor@gmail.com>
2019-07-01 14:53:45 +0900

    Treat org-agenda.

--- emacs/inits/07_org.el
+++ emacs/inits/07_org.el
@@ -1,2 +1,8 @@
 (setq org-startup-with-inline-images t)
+
+;; org-agenda
+(setq org-agenda-files '("/Users/honda.hiroaki/Dropbox/docs/SilverEgg/todo.org"))
+
+;; key-binds
 (global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
