dffd24dec3d8c072cc26ed56162aa505211a65e4
hiro0412 <from.endor@gmail.com>
2020-01-08 12:14:40 +0900

    Fix errors when emacs booting.

--- emacs/inits/06_markdown.el
+++ emacs/inits/06_markdown.el
@@ -11,6 +11,6 @@
 
 (autoload 'markdown-preview-mode "markdown-preview-mode.el" t)
 ;(setq markdown-preview-stylesheets (list "github.css"))
-(add-to-list 'markdown-preview-stylesheets "https://raw.githubusercontent.com/richleland/pygments-css/master/emacs.css")
+;(add-to-list 'markdown-preview-stylesheets "https://raw.githubusercontent.com/richleland/pygments-css/master/emacs.css")
 (setq markdown-preview-stylesheets (list "http://thomasf.github.io/solarized-css/solarized-light.min.css"))
 
