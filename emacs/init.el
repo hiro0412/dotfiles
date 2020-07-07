30f167247524fa93a88a7ed9c892c9e905a001a8
hiro0412 <from.endor@gmail.com>
2020-07-07 11:28:03 +0900

    [emacs] Initialize package.el in init.el

--- emacs/init.el
+++ emacs/init.el
@@ -49,6 +49,10 @@
 ;; 環境変数 PATH に exec-path を追加する。
 (setenv "PATH" (mapconcat 'identity exec-path ":"))
 
+;; initialize package.el
+(require 'package)
+(package-initialize)
+
 ;;; ==== init-loader ====
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
