ff8acc6766d8571449350595d918183fa80425ed
hiro0412 <from.endor@gmail.com>
2021-02-18 12:04:07 +0900

    [emacs] Add hl-todo.

---
+++ emacs/inits/05_hl-todo.el
@@ -0,0 +1,17 @@
+;; Settings for hl-todo mode
+;; -------------------------
+
+;; [github] https://github.com/tarsius/hl-todo
+
+
+(setq hl-todo-keyword-faces
+      '(("TODO"   . "#FF0000")
+        ("FIXME"  . "#FF0000")
+        ("DEBUG"  . "#A020F0")
+        ("GOTCHA" . "#FF4500")
+        ("STUB"   . "#1E90FF")))
+
+(define-key hl-todo-mode-map (kbd "C-c p") 'hl-todo-previous)
+(define-key hl-todo-mode-map (kbd "C-c n") 'hl-todo-next)
+(define-key hl-todo-mode-map (kbd "C-c o") 'hl-todo-occur)
+(define-key hl-todo-mode-map (kbd "C-c i") 'hl-todo-insert)
