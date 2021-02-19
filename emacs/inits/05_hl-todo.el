3a6360b66c2511f7342df33375dda6d57bcea193
hiro0412 <from.endor@gmail.com>
2021-02-19 11:20:19 +0900

    [emacs] update settings for hl-todo

--- emacs/inits/05_hl-todo.el
+++ emacs/inits/05_hl-todo.el
@@ -3,15 +3,16 @@
 
 ;; [github] https://github.com/tarsius/hl-todo
 
-
-(setq hl-todo-keyword-faces
-      '(("TODO"   . "#FF0000")
-        ("FIXME"  . "#FF0000")
-        ("DEBUG"  . "#A020F0")
-        ("GOTCHA" . "#FF4500")
-        ("STUB"   . "#1E90FF")))
-
-(define-key hl-todo-mode-map (kbd "C-c p") 'hl-todo-previous)
-(define-key hl-todo-mode-map (kbd "C-c n") 'hl-todo-next)
-(define-key hl-todo-mode-map (kbd "C-c o") 'hl-todo-occur)
-(define-key hl-todo-mode-map (kbd "C-c i") 'hl-todo-insert)
+(use-package hl-todo
+  :config
+  (setq hl-todo-keyword-faces
+	'(("TODO"   . "#FF0000")
+	  ("FIXME"  . "#FF0000")
+	  ("DEBUG"  . "#A020F0")
+	  ("GOTCHA" . "#FF4500")
+	  ("STUB"   . "#1E90FF")))
+  (define-key hl-todo-mode-map (kbd "C-c p") 'hl-todo-previous)
+  (define-key hl-todo-mode-map (kbd "C-c n") 'hl-todo-next)
+  (define-key hl-todo-mode-map (kbd "C-c o") 'hl-todo-occur)
+  (define-key hl-todo-mode-map (kbd "C-c i") 'hl-todo-insert)
+  )
